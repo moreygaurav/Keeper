7e7a20ec9beec452b0d6261fc15b6a284024524c
GauravMorey <gaurav1234pmorey@gmail.com>
2026-01-30 16:13:16 +0530

    imported pakage hash router and added gh-pages so that my react project will work properly on github and it wont show white or blank screen after deployment on github

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Components/App";
 import "./Style.css"
-
+import hashrouter from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <hashrouter>
     <App />
-  </React.StrictMode>
+  </hashrouter>
 );
